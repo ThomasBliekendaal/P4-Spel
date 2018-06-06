1969169b216099e1302cddb52607072af9884f80
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2018-06-06 13:00:37 +0200

    Equipment bleh

---
+++ P4 Spel/Assets/Scripts/Equipment/GunDeploy.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunDeploy : MonoBehaviour
+{
+
+    public GameObject weapon;
+    public GameObject activeWeapon;
+    public GameObject rotationPoint;
+    public bool active;
+    public List<Transform> Enemy;
+    public float range;
+    public WeaponPartInfo info;
+    public Vector3 r;
+
+    public bool activeFire;
+
+    public void Start()
+    {
+        StartCoroutine(StartTimer());
+    }
+
+    public IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(2);
+        transform.rotation = new Quaternion(0, transform.rotation.y, 0, transform.rotation.w);
+        GameObject g = Instantiate(weapon, rotationPoint.transform.position, transform.rotation, rotationPoint.transform);
+        rotationPoint.transform.rotation = new Quaternion(0, transform.rotation.y, 0, rotationPoint.transform.rotation.w);
+        activeWeapon = g;
+        Destroy(g.GetComponent<Weapon>());
+        info = activeWeapon.GetComponent<WeaponPartInfo>();
+        active = true;
+    }
+
+    public void Update()
+    {
+        Enemy = new List<Transform>();
+        if (active)
+        {
+            activeWeapon.transform.position = Vector3.MoveTowards(activeWeapon.transform.position, rotationPoint.transform.position, Time.deltaTime);
+            Collider[] colliders = Physics.OverlapSphere(transform.position, range);
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (colliders[i].gameObject.tag == "Enemy")
+                {
+                    Enemy.Add(colliders[i].transform);
+                }
+            }
+
+            if (Enemy.Count != 0)
+            {
+                if (!activeFire)
+                {
+                    activeFire = true;
+                    StartCoroutine(Fire());
+                }
+                rotationPoint.transform.LookAt(Enemy[0]);
+            }
+            else if (Enemy.Count == 0)
+            {
+                activeFire = false;
+            }
+        }
+    }
+
+    public IEnumerator Fire()
+    {
+        r = new Vector3(Random.Range(5 / -info.accuracy, 5 / info.accuracy), Random.Range(2 / -info.accuracy, 2 / info.accuracy), Random.Range(2 / -info.accuracy, 2 / info.accuracy));
+        activeWeapon.transform.Translate(0, 1 / info.stability, 0);
+        GameObject g = Instantiate(info.ammoType.projectile, rotationPoint.transform.position, rotationPoint.transform.rotation);
+        g.transform.Rotate(r);
+        g.GetComponent<FriendlyBullet>().type = activeWeapon.GetComponent<GripType>().element;
+        g.GetComponent<FriendlyBullet>().damage = info.damage;
+        g.GetComponent<FriendlyBullet>().firerate = info.fireRate;
+        Destroy(g, 5);
+        yield return new WaitForSeconds(1 / info.fireRate);
+        if (activeFire)
+        {
+            StartCoroutine(Fire());
+        }
+    }
+}
---
+++ P4 Spel/Assets/Scripts/Equipment/WallTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WallTimer : MonoBehaviour
+{
+    public Image image;
+
+    public void Start()
+    {
+        Destroy(gameObject, 12);
+        StartCoroutine(StartTimer());
+    }
+
+    public IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(2);
+        StartCoroutine(ReloadVisual());
+    }
+
+    public IEnumerator ReloadVisual()
+    {
+        image.fillAmount -= 0.1f * Time.deltaTime;
+        yield return new WaitForSeconds(0.01f);
+        StartCoroutine(ReloadVisual());
+    }
+}
