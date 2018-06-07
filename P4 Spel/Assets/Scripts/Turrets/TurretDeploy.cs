0a3e21d4a0999fb69747a731023989361b3b2fcf
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2018-06-07 15:30:40 +0200

    TurretDeploy

---
+++ P4 Spel/Assets/Scripts/Turrets/TurretDeploy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretDeploy : MonoBehaviour {
+
+    public GameObject impact;
+    public GameObject turret;
+    public bool falling;
+    public bool active;
+    public GameObject activeTurret;
+    public float distance;
+
+	void Update () {
+        if (active)
+        {
+            active = false;
+            SetTurret();
+        }
+        if (falling)
+        {
+            distance = Vector3.Distance(transform.position + (Vector3.up * 0.9f), activeTurret.transform.position);
+            activeTurret.transform.Translate(-Vector3.up * 20 * Time.deltaTime);
+            if(distance <= 0.5f)
+            {
+                GameObject g = Instantiate(impact, transform.position + (Vector3.up * 0.9f), Quaternion.identity);
+                    Destroy(g, 3);
+                falling = false;
+                activeTurret.transform.position = transform.position + (Vector3.up * 0.9f);
+            }
+        }
+	}
+
+    public void SetTurret()
+    {
+        activeTurret = Instantiate(turret, transform.position + Vector3.up * 30,Quaternion.identity);
+        falling = true;
+    }
+}
