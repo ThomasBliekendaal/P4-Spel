7de8dc7141b33afe3aaf7d1a783916bd6986045a
ThomasBliekendaal <33858982+ThomasBliekendaal@users.noreply.github.com>
2018-06-13 14:56:43 +0200

    ui yippie

--- P4 Spel/Assets/Scripts/GeneralUi.cs
+++ P4 Spel/Assets/Scripts/GeneralUi.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GeneralUi : MonoBehaviour {
+    public GameObject escapeMenu;
 
 	// Use this for initialization
 	void Start () {
-		
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown("Escape"))
+        {
+            escapeMenu.SetActive(!escapeMenu.activeInHierarchy);
+        }
 	}
 }
