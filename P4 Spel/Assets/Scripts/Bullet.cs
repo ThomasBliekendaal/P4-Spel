a9b347e1f79c619e029ed35f1af989dc89fe59bd
Stefan <stefanherder2001@gmail.com>
2018-06-20 11:54:20 +0200

    Changes

--- P4 Spel/Assets/Scripts/Bullet.cs
+++ P4 Spel/Assets/Scripts/Bullet.cs
@@ -7,31 +7,57 @@ public class Bullet : MonoBehaviour
 
     public float speed = 10;
     public float damage;
+    public Color[] possibleColors;
+    private Color active;
+    public TrailRenderer tR;
+    public bool bouncing;
 
-    void Start()
+    private void Awake()
     {
-        Destroy(gameObject,8);
+        Physics.IgnoreLayerCollision(0, 11);
+        TrailRenderer tR = gameObject.GetComponent<TrailRenderer>();
+        Renderer rM = gameObject.GetComponent<Renderer>();
+        active = possibleColors[Random.Range(0, possibleColors.Length)];
+        rM.material.color = active;
+        tR.material.color = active;
+        tR.material.SetColor("_EmissionColor", active);
+        Destroy(gameObject, 7);
+        Rigidbody rB = gameObject.GetComponent<Rigidbody>();
+        rB.AddForce(10, 0, 0);
     }
 
     void Update()
     {
-        transform.Translate(new Vector3(0, 0, speed) * Time.deltaTime);
+        if (!bouncing)
+        {
+            transform.Translate(new Vector3(0, 1, speed*2) * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(new Vector3(0, 0, -speed) * Time.deltaTime);
+        }
     }
 
     public void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if(collision.gameObject.tag != "Enemy")
+        {
+            if(collision.gameObject.tag != "Floor")
+            {
+                bouncing = true;
+                Rigidbody rB = gameObject.GetComponent<Rigidbody>();
+                rB.AddForce(10, 0, 0);
+                rB.mass = 1.1f;
+                Destroy(gameObject, 0.1f);
+            }
+        }
+        if(collision.gameObject.tag == "Floor")
+        {
+            Destroy(gameObject,0.4f);
+        }
+        if(collision.gameObject.tag == "Player")
         {
             collision.gameObject.GetComponent<PlayerScript>().DoDam(damage);
-            Destroy(gameObject);
-        }
-        if (collision.gameObject.tag == "Untagged")
-        {
-            Destroy(gameObject);
-        }
-        if (collision.gameObject.tag == "Enemy")
-        {
-            Physics.IgnoreCollision(gameObject.gameObject.GetComponent<Collider>(), collision.gameObject.GetComponent<Collider>());
         }
     }
 }
