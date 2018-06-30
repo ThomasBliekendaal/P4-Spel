c608e135d3fb2f10ec531e2f06ca2542d3394f5f
ThomasBliekendaal <33858982+ThomasBliekendaal@users.noreply.github.com>
2018-06-30 18:38:11 +0200

    rocketlauncher balancing progress

--- P4 Spel/Assets/Scripts/Weapons/WeaponSwap.cs
+++ P4 Spel/Assets/Scripts/Weapons/WeaponSwap.cs
@@ -48,7 +48,7 @@ public class WeaponSwap : MonoBehaviour {
                 current = weapons.Count - 1;
             }
             weapons[current].SetActive(true);
-            GameObject.FindWithTag("CheatMenu").GetComponent<CheatScript>().weapon = weapons[current];
+            //GameObject.FindWithTag("CheatMenu").GetComponent<CheatScript>().weapon = weapons[current];
             weaponInfoSlot[current].highlight.SetActive(true);
             weapons[current].GetComponent<Weapon>().active = false;
         }
