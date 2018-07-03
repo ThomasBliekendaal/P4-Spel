2e450c6ce0666a1a0c101e46b4c179fa37c5cad0
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2018-07-03 02:08:25 +0200

    push, lyght fhix, and ui

---
+++ P4 Spel/Assets/Scripts/StartUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartUI : MonoBehaviour {
+
+    public GameObject weaponUI;
+
+    public void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.SetActive(false);
+            weaponUI.SetActive(true);
+        }
+    }
+}
